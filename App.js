0b824995ae5029d066895d8f6952c7cdac3a811b
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2022-09-10 17:11:41 +0200

    Remove home page from src/screens and from App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/home';
+import * as React from 'react';
 import MyFoodListScreen from './src/screens/my-food-list';
 
 const Stack = createNativeStackNavigator();
@@ -13,9 +12,6 @@ export default function App() {
         <Stack.Screen
           name="Lista de alimentos"
           component={MyFoodListScreen} />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
