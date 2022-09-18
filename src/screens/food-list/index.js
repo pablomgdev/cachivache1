67a8804c6cd7992dd080922e0372c81dce431ea0
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2022-09-18 18:24:53 +0200

    Add some example styles to food list elements

--- src/screens/food-list/index.js
+++ src/screens/food-list/index.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { FlatList, View, Text } from 'react-native';
+import {
+  FlatList,
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
 
+// TODO: Move data to another place
 const DATA = [
   { id: 1, name: 'Chorizo' },
   { id: 2, name: 'Huevos' },
@@ -10,11 +16,29 @@ const DATA = [
 // TODO: refactor: move to components and so on...
 function FoodCard(props) {
   return (
-    <View>
-      <Text>Food: {props.name}</Text>
+    <View style={styles.card}>
+      <Text style={styles.cardTitle}>{props.name}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  // TODO: add global container centered with 90% of width.
+  //  Maybe a styles prop has to be passed to the component.
+  //  See how to center something in react native
+  card: {
+    backgroundColor: '#4287f5', // TODO: use a degraded color
+    borderRadius: 15,
+    padding: 10,
+    margin: 3,
+    marginHorizontal: 10,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  }
+});
 
 function MyFoodListScreen() {
   const renderItem = ({ item }) => {
@@ -24,10 +48,7 @@ function MyFoodListScreen() {
   }
 
   return (
-    <FlatList
-      data={DATA}
-      renderItem={renderItem}
-    />
+    <FlatList data={DATA} renderItem={renderItem} />
   );
 }
 
